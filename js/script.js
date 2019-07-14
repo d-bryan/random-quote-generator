93a1ec2aa3faf5080cff3092769a1028d831b980
d-bryan <dylan.g.bryan@gmail.com>
2019-07-14 19:12:28 -0400

    "Final updates with all code working properly and the background changing colors as planned."

--- js/script.js
+++ js/script.js
@@ -36,7 +36,6 @@ const quotes = [
     citation: "The Wizard of Oz",
     year: 1939,
     used: false,
-    background_color: "coral",
     tag: "Family"
   },
   {
@@ -47,7 +46,6 @@ const quotes = [
     citation: "Star Wars",
     year: 1977,
     used: false,
-    background_color: "black",
     tag: "Family"
   },
   {
@@ -58,7 +56,6 @@ const quotes = [
     citation: "The Adventures of Sherlock Holmes",
     year: 1939,
     used: false,
-    background_color: "burlywood",
     tag: "Drama"
   },
   {
@@ -69,7 +66,6 @@ const quotes = [
     citation: "The Silence of the Lambs",
     year: 1991,
     used: false,
-    background_color: "#b9072d",
     tag: "Horror"
   },
   {
@@ -80,7 +76,6 @@ const quotes = [
     citation: "Caddyshack",
     year: 1980,
     used: false,
-    background_color: "#97cf2e",
     tag: "Comedy"
   },
   {
@@ -91,47 +86,12 @@ const quotes = [
     citation: "Dr. No",
     year: 1962,
     used: false,
-    background_color: "##535353",
     tag: "Action"
   }
 ];
-
-var wizard = quotes[0].quote;
-var coral = quotes[0].background_color;
-var starWars = quotes[1].quote;
-var black = quotes[1].background_color;
-var sherlock = quotes[2].quote;
-var burlywood = quotes[2].background_color;
-var hannibal = quotes[3].quote;
-var brickred = quotes[3].background_color;
-var caddy = quotes[4].quote;
-var turfgreen = quotes[4].background_color;
-var bond = quotes[5].quote;
-var steel = quotes[5].background_color;
-const getPQuote = document.querySelector('p.quote');
-const getPSource = document.querySelector('p.source');
-const getSCitation = document.getElementsByClassName('citation');
-const getSYear = document.getElementsByClassName('year');
-const getSActor = document.getElementsByClassName('actor');
-const getSTag = document.getElementsByClassName('tag');
-const getPElement = document.querySelector('p');
-const nextTarget = getPSource.nextSibling;
-
-
-/**
- * Test function to print out the objects to the console to ensure they are operating properly
- */
-// console.log(quotes);
-// console.log(quotes[0]);
-// console.log(quotes[0].quote);
-function printObject(quoteObj) {
-  for (let i = 0; i < quoteObj.length; i ++) {
-    console.log(quoteObj[i].quote);
-    console.log(quoteObj[i].background_color);
-  }
-}
-// console.log(quotes[0].background_color);
-// printObject(quotes);
+const backgroundColor = [
+  "coral", "black", "burlywood", "#b9072d", "#97cf2e", "#535353"  
+];
 
 /**
  * Helper function used to print to the page.
@@ -153,34 +113,19 @@ function print(message) {
     return Math.floor(Math.random() * Math.floor(arr.length));
  }
 
-/**
- * 
- * Function used to set the items used property to false.
- * @param {Function} arr 
- * - Uses `forEach` to loop through `items` in array and set them to `false`
- */
- function setArray (arr) {
-   arr.forEach(item => item.used = false); 
- }
 
 /**
- * Function used to retrieve unused quotes then pass to callback function.
- * @param {Function} arr
- * - Creates variable `unusedQuote` to contain the filter for unused quotes
- * @param {Function} callback
- * - `return`s the `callback` of `unusedQuote` to the Function. 
+ * Function that generates a random color from the Background colors Array.
+ * @param {Function} bgColorArr 
+ * - Passes backgroundColors array to function
+ * - `num` - gets random integer to pass into placeholder of array
+ * - `color` - becomes the color to be passed into the random background color.
  */
-function getUnusedQuote (arr, callback) {
-  let unusedQuote = arr.filter(arr => !arr.used);
-  return callback(unusedQuote);
+function getRandomColor (bgColorArr) {
+  let num = getRandomInt(backgroundColor);
+  let color = bgColorArr[num];
+  return color;
 }
-
-
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
 
 /**
  * 
@@ -197,115 +142,6 @@ function getRandomQuote(quoteObj) {
   // console.log(num);
   return changeQuote;
 }
-console.log(getRandomQuote(quotes));
-
-/**
- * For Each Function that loops over all the properties of the objects to be used
- * for building the other parts to the quote.
- * @param {Function} element
- * - `element` passes the property to the function to be stored in building to the page
- * -`sourceArr` {Array} of source attributes
- * -`actorArr` {Array} of actor attributes
- * -`citationArr` {Array} of citation attributes
- * -`yearArr` {Array} of year attributes
- * -`background_colorArr` {Array} of background color attributes
- * -`tagArr` {Array} of tag attributes
- */
-const sourceArr = [];
-const actorArr = [];
-const citationArr = [];
-const yearArr = [];
-const background_colorArr = [];
-const tagArr = [];
-  quotes.forEach(function(element){
-    let source;
-    let actor;
-    let citation;
-    let year;
-    let background_color;
-    let tag;
-
-    source = element.source;
-    actor = element.actor;
-    citation = element.citation;
-    year = element.year;
-    background_color = element.background_color;
-    tag = element.tag;
-
-    sourceArr.push(source);
-    actorArr.push(actor);
-    citationArr.push(citation);
-    yearArr.push(year);
-    background_colorArr.push(background_color);
-    tagArr.push(tag);
-  });
-
-
-
-
-
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-function printQuote () {
-  let randQuote = getRandomQuote(quotes);
-  let buildToPage = '';
-  buildToPage += '<p class="quote">' + randQuote["quote"] + '</p>';
-  buildToPage += '<p class="source">' + randQuote["source"] +'</p>';
-  for (var i = 0; i < quotes.length; i += 1) {
-    var quoteObj = quotes[i];
-    // buildToPage += '<p class="quote">' + randQuote + '</p>';
-    console.log(quoteObj.quote.toString());
-  }
-
-
-
-
-
-  // for (key in quotes){
-  //   if (quotes[key].quote === randQuote){
-  //     buildToPage += getPSource.textContent = quotes[key].source + ' ';
-  //     if (quotes[key].hasOwnProperty(quotes[key].source))
-  //     buildToPage += getSCitation.textContent = quotes[key].citation + ' ';
-  //     buildToPage += getSYear.textContent = quotes[key].year + ' ';
-  //     buildToPage += getSActor.textContent = quotes[key].actor + ' ';
-  //     buildToPage += getSTag.textContent = quotes[key].tag + ' ';
-      
-
-  //   }
-print(buildToPage);
-
-}
-console.log(getRandomQuote(quotes).toString());
-printQuote();
-
-// for (i = 0; i < quotes.length; i ++) {
-//     let source = quotes[i].source;
-//     console.log(source);
-// }
-
-// let randQuote = getRandomQuote(quotes);
-// console.log(randQuote);
-// if (quotes.quote === randQuote) {
-//     let numSource = quotes.source;
-    // console.log(quotes.source);
-    // console.log(quotes[numSource].source);
-// }
-
-//console.log(quotes[0].source);
-// console.log(quotes);
-
 
 /**
  * Function that calls on `window.setInterval` to change a callback function.
@@ -313,35 +149,50 @@ printQuote();
  * @param {Function}  `time` in milliseconds for how long it will be delayed
  */
 function changeBackgroundColor () { 
-var intervalID = setInterval(switchColors, 5000);
-return intervalID;
-}
-/**
- * Build a switch statement to switch between the background colors whenever the
- * `window.setInterval` timesout.
- * @param {Switch} quote
- */
-function switchColors (){
-  switch (quote) {
-      case wizard:
-        document.body.style.backgroundColor = coral;
-        break;
-      case starWars:
-        document.body.style.backgroundColor = black;
-        break;
-      case sherlock:
-        document.body.style.backgroundColor = burlywood;
-        break;
-      case hannibal:
-        document.body.style.backgroundColor = brickred;
-        break;
-      case caddy:
-        document.body.style.backgroundColor = turfgreen;
-        break;
-      default:
-        document.body.style.backgroundColor = "#36b55c";
+  var intervalID = setInterval(function switchColors (randColor){
+      randColor = getRandomColor(backgroundColor);
+      console.log(randColor);
+      document.body.style.backgroundColor = randColor;
+      return randColor;
+    }, 8000);
+  return intervalID;
   }
+  changeBackgroundColor();
+ 
+/**
+ * Function that prints random quotes to the page with their corresponding attributes.
+ * - `buildToPage` - Empty string that consecutively builds the template for HTML
+ * - `randQuote` - Holds the randomly genrated object for usage in building
+ * - `print` {Function} - Helper function that places the build to the page.
+ */
+
+function printQuote () {
+  let randQuote = getRandomQuote(quotes);
+  let buildToPage = '';
+  buildToPage += '<p class="quote">' + randQuote["quote"] + '</p>';
+  buildToPage += '<p class="source">' + randQuote["source"];
+  if (randQuote["citation"] !== undefined) {
+    buildToPage += '<span class="citation">' + randQuote["citation"] +'</span>';
+  }
+  if (randQuote["year"] !== undefined) {
+    buildToPage += '<span class="year">' + randQuote["year"] + '</span>';
+  }
+  if (randQuote["actor"] !== undefined) {
+    buildToPage += '<span class="actor">' + randQuote["actor"] + '</span>';
+  }
+  if (randQuote["tag"] !== undefined) {
+    buildToPage += '<span class="tag">' + randQuote["tag"] + '</span>';
+  }
+  buildToPage += '</p>';
+  
+print(buildToPage);
+
 }
+printQuote();
+
+
+
+
 
 /***
   When the "Show another quote" button is clicked, the event listener 
@@ -349,7 +200,6 @@ function switchColors (){
   function. So do not make any changes to the line of code below this 
   comment.
 ***/
-// !!! Remember to UNCOMMENT THIS LINE OF CODE !!!
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
